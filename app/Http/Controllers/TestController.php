e931d82169aa132eca6da9ac83a9e495efe30e56
Duy271 <Nguyenkhanhduy.learn@gmail.com>
2024-10-15 01:29:35 +0700

    first commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\san_pham;
-
+use GuzzleHttp\Psr7\Response;
 
 class TestController extends Controller
 {
     //
     public function tat_ca_san_pham()
     {
-        $sanPhams = san_pham::with(['loai_san_pham', 'nha_cung_cap', 'khuyen_mai_san_pham', 'product_images'])->get();
-        dd($sanPhams->toArray());
+        $sanPhams = san_pham::with(['loai_san_pham', 'nha_cung_cap', 'khuyen_mai_san_pham', 'anh_san_pham'])->get();
+      
+        return response()->json($sanPhams);
     }
 }
