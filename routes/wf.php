bd20cf66d4269110f3e2fd68822e4ef1235c45c7
Duy271 <Nguyenkhanhduy.learn@gmail.com>
2024-11-28 21:07:44 +0700

    28/11

--- routes/wf.php
+++ routes/wf.php
@@ -5,4 +5,6 @@ Route::post('/suaanhsanpham',[App\Http\Controllers\WFController::class,'suaAnhSa
 Route::get('/laybienthetheosanpham',[App\Http\Controllers\WFController::class,'layBienTheTheoSanPham']);
 Route::post('/themanhsanpham',[App\Http\Controllers\WFController::class,'themAnhSanPham']);
 Route::post('/xoaanhsanpham',[App\Http\Controllers\WFController::class,'xoaAnhSanPham']);
+
 Route::post('/themdondat',[App\Http\Controllers\WFController::class,'themDonDat']);
+Route::get('/laydondat',[App\Http\Controllers\WFController::class,'layDonDat']);
